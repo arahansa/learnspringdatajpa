e4468f0df0cdc0b97e6656d5bfa22a861c133c99
KangYong <arahansa@naver.com>
2015-03-20 14:10:56 +0900

    조건 가진 리스트 뽑아보기

--- src/main/java/com/example/repository/JustOneRepository.java
+++ src/main/java/com/example/repository/JustOneRepository.java
@@ -1,5 +1,8 @@
 package com.example.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.domain.JustOne;
@@ -7,4 +10,5 @@ import com.example.domain.JustOne;
 
 public interface JustOneRepository extends JpaRepository<JustOne, Long>{
 	public JustOne findByMessage(String message);
+	public List<JustOne> findBydateJoinBeforeAndMessage(Date date, String message);
 }
--- src/test/java/com/example/repository/JustOneRepositoryTest.java
+++ src/test/java/com/example/repository/JustOneRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.example.repository;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -18,7 +22,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.example.SampleController;
 import com.example.domain.JustOne;
@@ -112,6 +115,34 @@ public class JustOneRepositoryTest {
 		JustOne one = repository.findByMessage(HELLO_WORLD);
 		assertNotNull(one);
 		assertEquals(one.getMessage(), HELLO_WORLD);
+	}
+	
+	@Test
+	public void 날짜이전_특정메시지로찾기() throws Exception {
+		repository.deleteAll();
+		/**주의 * 자바 날짜는 자바8, joda time 같은 걸로 해주는게 더 좋다고 합니다.
+		 * 여기선 그냥 간단하게 java date 로 이미 가버렸습니다;; 삐질  */
+		//먼저 데이터를 삽입하고
+		Calendar calendar;	
+		for(int i=0;i<10;i++){
+			calendar = new GregorianCalendar(2010+i,1,1,13,24,56);
+			JustOne one = new JustOne();
+			one.setDateJoin(calendar.getTime());
+			one.setMessage(i%2==0? HELLO_WORLD: _1234);
+			repository.save(one);	
+		}
+		//기준 데이터를 가져와서 특정날짜이전과 해당 메시지에 해당하는 리스트를 뽑아봅니다.
+		calendar = new GregorianCalendar(2015,1,1,13,24,56);
+		List<JustOne> getList = 
+		repository.findBydateJoinBeforeAndMessage(calendar.getTime(), HELLO_WORLD);
+		//그리고 검사
+		int year=2010;
+		for (JustOne justOne : getList) {	
+			calendar.set(year,1,1,13,24,56);
+			assertEquals(justOne.getMessage(), HELLO_WORLD);
+			assertEquals(justOne.getDateJoin(), calendar.getTime());
+			year+=2;
+		}
 	}
 	
 	
